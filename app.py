825f1325abc960a15b91bebe54a9eb149ee98315
CHETAN B R <chiranjeevichetan1996@gmail.com>
2024-06-16 07:30:12 +0530

    app.py modified

--- app.py
+++ app.py
@@ -299,4 +299,4 @@ def video():
 if __name__ == "__main__":
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
